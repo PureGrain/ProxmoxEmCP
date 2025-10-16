11b2eb531a8c4f9e6c7abfd38a7bd1fc87cf43ec
AntsAble <challon.holt@gmail.com>
2025-10-16 18:28:52 -0400

    test: remove environment variable test that fails in CI without secrets

--- tests/test_env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def test_env_vars():
-    assert os.getenv("PROXMOX_HOST") is not None, "PROXMOX_HOST should be set"
-    assert os.getenv("PROXMOX_TOKEN_NAME") is not None, "PROXMOX_TOKEN_NAME should be set"
-    assert os.getenv("PROXMOX_TOKEN_VALUE") is not None, "PROXMOX_TOKEN_VALUE should be set"
